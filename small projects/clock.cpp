fc2be38f7b5b40e1e95cf3b71282df89065f0939
Prakhar Pratap <prakharpratap2001@gmail.com>
2024-02-21 16:33:56 +0530

    clock

---
+++ small projects/clock.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+int main() {
+    int h, m, s, err;
+    err = 0;
+    
+    while (err == 0) {
+        cout << "Enter Hour: " << endl;
+        cin >> h;
+        cout << "Enter Minutes: " << endl;
+        cin >> m;
+        cout << "Enter Seconds: " << endl;
+        cin >> s;
+        if (h < 24 && m < 60 && s < 60)
+            err++;
+        else 
+            system("clear");
+    }
+    while (true) {
+        system("clear");
+        cout << h << ":" << m << ":" << s << endl;
+        sleep(1);
+        s++;
+        if (s > 59) {
+            s = 0;
+            m++;
+        }
+        if (m > 59) {
+            m = 0;
+            h++;
+        }
+        if (h > 24) {
+            h = 0;
+        }
+    }
+    return 0;
+}
